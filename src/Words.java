863497533a95ed45e50e5f8241b6320f0e462b20
FoxMary <Fox.mary@rambler.ru>
2019-05-15 15:44:19 +0300

    DZ_3

--- src/Words.java
+++ src/Words.java
@@ -4,9 +4,9 @@ public class Words {
     public static void main(String[] args) {
         String[] words = {"Дуб", "Клен", "Липа", "Дуб", "Береза", "Дуб", "Дуб", "Тополь", "Клен", "Дуб", "Тополь", "Липа"};
         HashMap<String, Integer> hm = new HashMap<>();
-        for(int i = 0; i < words.length; i++){
-            Integer word = hm.get(words[i]);
-            hm.put(words[i], word == null ? 1 : word + 1);
+        for (String s : words) {
+            Integer word = hm.get(s);
+            hm.put(s, word == null ? 1 : word + 1);
         }
         System.out.println(hm);
     }
